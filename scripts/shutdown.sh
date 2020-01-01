328f8ba5b0aced431f26103f1df32e30ac74f15c
Keith Gregory <contact@kdgregory.com>
2020-01-01 08:19:23 -0500

    shutdown.sh: delete buckets before deleting stack

--- scripts/shutdown.sh
+++ scripts/shutdown.sh
@@ -9,13 +9,13 @@
 
 STACK_ID=$1
 
-# grab this before we terminate the stack; can get it afterward only if using actual ID
 BUCKETS=$(aws cloudformation describe-stacks --stack-name $STACK_ID \
           --output table --query 'Stacks[].Parameters[].[ParameterKey,ParameterValue]' \
           | grep BucketName | sed -e 's/.* |  *//' | sed -e 's/ .*//')
 
-aws cloudformation delete-stack --stack-name $STACK_ID
-
 for b in $BUCKETS ; do
     aws s3 rb --force s3://$b
     done
+
+# this should complete now that the buckets have been forcibly removed
+aws cloudformation delete-stack --stack-name $STACK_ID
